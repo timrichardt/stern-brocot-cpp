c3419e57f507749a964a2bc372df2fb5739c1154
Tim Richardt <tim@richardt.io>
2025-03-15 16:29:58 +0100

    fix: WIP chaining arithmetic operations

--- main.cpp
+++ main.cpp
@@ -21,18 +21,20 @@ int main() {
 	Number x = parse_SSB("RRRR");
 	Number y = parse_SSB("R");
 	Number u = parse_SSB("RRRRLL");
-	Number f = parse_SSB("R");
+	Number f = parse_SSB("");
+	Number g;
 	// Number z = add(x,y); // bihom(Bihom{0, 1, 1, 0, 0, 0, 0, 1}, std::move(x), std::move(y));
 	// std::unique_ptr<Number> z = std::make_unique<Number>(x + y);
-	Number z = x / y;
+	Number z = x + y;
+	// g = z + f;
 	// Number w = z * f;
 
 	std::cout << z << std::endl;
 	
 	// if (z == u) std::cout << "yip" << std::endl;
-	if (z == u) {
-		std::cout << "yip" << std::endl;
-	}
+	// if (z == u) {
+	// 	std::cout << "yip" << std::endl;
+	// }
 	
 	// std::unique_ptr<Number> w = std::make_unique<Number>(*z + u);
 	// std::cout << z << std::endl;
--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -3,7 +3,10 @@
 #include "tree.h"
 
 Number Number::operator+(Number &other) {
-  return bihom(Bihom{0, 1, 1, 0, 0, 0, 0, 1}, *this, other);
+  Number a = Number{sign, seq->clone()};
+  Number b = Number{other.sign, other.seq->clone()};
+
+  return bihom(Bihom{0, 1, 1, 0, 0, 0, 0, 1}, a, b);
 }
 
 Number Number::operator-(Number &other) {
